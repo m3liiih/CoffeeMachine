56552695b3215435b3bb36c6977572abf2e08dba
m3liiih <kivancahmetmelih@gmail.com>
2024-12-06 23:21:45 +0300

    Added a function to calculate if machine can make certain number of cups of coffee based on ingredients needed per cup

--- CoffeeMachine.py
+++ CoffeeMachine.py
@@ -6,5 +6,17 @@ def make_coffee():
           "Pouring the coffee into a cup...\n"
           "Adding some milk into the cup...\n"
           "Coffee is ready!")
-
 make_coffee()
+
+def ingredients_per(cups):
+      water_ml = cups * 200
+      milk_ml = cups * 50
+      beans_g = cups * 20
+
+      print(f"For {cups} cups of coffee you will need:")
+      print(f"{water_ml} ml of water")
+      print(f"{milk_ml} ml of milk")
+      print(f"{beans_g} g of coffee beans")
+
+cups = int(input("How many cups of coffee will you need: "))
+ingredients_per(cups)
