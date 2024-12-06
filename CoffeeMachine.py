f8648b0d356dc24f83659370a1b2927c1bc0d726
m3liiih <kivancahmetmelih@gmail.com>
2024-12-06 23:04:27 +0300

    Add CoffeeMachine.py

---
+++ CoffeeMachine.py
@@ -0,0 +1,10 @@
+def make_coffee():
+    print("Starting to make coffee...\n"
+          "Grinding coffee beans...\n"
+          "Boiling water...\n"
+          "Mixing the boiled water with ground coffee beans...\n"
+          "Pouring the coffee into a cup...\n"
+          "Adding some milk into the cup...\n"
+          "Coffee is ready!")
+
+make_coffee()
