797189ae48832fcc091bfea9310fce62cf008cb5
m3liiih <kivancahmetmelih@gmail.com>
2024-12-08 21:56:20 +0300

    edited intro text

--- CoffeeMachine.py
+++ CoffeeMachine.py
@@ -2,10 +2,7 @@ def make_coffee():
     print("Starting to make coffee...\n"
           "Grinding coffee beans...\n"
           "Boiling water...\n"
-          "Mixing the boiled water with ground coffee beans...\n"
-          "Pouring the coffee into a cup...\n"
-          "Adding some milk into the cup...\n"
-          "Coffee is ready!")
+          "Coffee is ready to mix!")
 make_coffee()
 print()
 
